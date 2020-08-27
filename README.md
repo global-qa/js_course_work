a81e8902a1393c569e63c24ce8c43792571103ee
Eugene.Korogodsky <fuckgithub2>
2020-08-27 15:01:29 +0300

    Solved for 1 dice

--- README.md
+++ README.md
@@ -4,3 +4,24 @@
 $ yarn instal
 $ yarn run test
 ```
+Scheme is here: https://drive.google.com/file/d/1qi0KGLFjAeioQtDUMCO10n8-HnljikUJ/view?usp=sharing
+
+##Task:
+Course Work
+Language - JavaScript
+
+Create tests(using REST API) for:
+
+GET: https://www.random.org/integers/?num=1&min=1&max=1&col=1&base=10&format=plain&rnd=new  
+
+To check the following hypothesis: "For large number of rolls (number of rolls > 1000) distribution of dice points strives to uniform distribution." 
+
+Requirements: tests should check that maximum deviation of dice results is within 5% First test: for 1 dice Second test: for 2 dices (total roll points is a sum of 2 dices)
+
+Hint 1: try 1000, 5000, 10000 number of rolls and see how distribution changes 
+
+Hint 2: think about how to implement the solution in the most efficient way
+
+------
+
+All information you should add to your GitHub repository, where you have uploaded your test cases and the cod
--- specs/courseWork.spec.js
+++ specs/courseWork.spec.js
@@ -1,14 +1,76 @@
 const superagent = require('superagent');
 require('superagent-retry-delay')(superagent);
 const agent = superagent.agent();
-describe.only('Test', async function() {
-    it('Get all rolls', async function() {
-        let res = await agent
-            .get("https://www.random.org/integers/?num=1000&min=1&max=6&col=1&base=10&format=plain&rnd=new")
-            .then((res) => {
-                return res;
-            })
-        let allRolls = await res.text.split("\n")
-        console.log("QQQQ", allRolls)
-    });
+const initialQtty = 1000;
+const maxQtty = 10000;
+const maxDeviation = 5;//%
+const errMsgDailyLimit = 'Error: You have used your quota of random bits for today.  See the quota page for details.';
+//const dicesQtty = 1;//?
+const howQuick = 2.5;
+var currentQtty = initialQtty;
+var currentDeviation = 10;
+var iterationNumber = 0;
+
+describe.only('Test', async function () {
+    it('Get all rolls', async function () {
+        while (currentDeviation > 1 + maxDeviation / 100) {
+            currentQtty < maxQtty
+                ? console.log(`Iteration No: ${++iterationNumber}, we are trying ${currentQtty}`)
+                : console.log(`Iteration No: ${++iterationNumber}, we are trying the maximum: ${currentQtty = maxQtty}`);
+
+            let res = await agent
+                .get(`https://www.random.org/integers/?num=${currentQtty}&min=1&max=6&col=1&base=10&format=plain&rnd=new`)
+                .then((res) => {
+                    return res;
+                });
+
+            if (res.text.toString().includes(errMsgDailyLimit)) {
+                console.error(`\x1B[33mOops... Unfortunately you can't make this API-request today anymore, server reported:
+\x1b[1m\x1B[31m${errMsgDailyLimit}\x1b[0m
+\x1B[33mYou can try to repeat tomorrow or change your IP\x1b[0m`);
+                exit;//не знаю, як завалити тест, щоб закінчився із помилкою
+            }
+
+            let allRolls = await res.text.split("\n");
+            let scoreOfRolls = [0, 0, 0, 0, 0, 0];
+            for (let i = 0; i < allRolls.length; i++) {
+                switch (allRolls[i]) {
+                    case "1":
+                        scoreOfRolls[0]++;
+                        break;
+                    case "2":
+                        scoreOfRolls[1]++;
+                        break;
+                    case "3":
+                        scoreOfRolls[2]++;
+                        break;
+                    case "4":
+                        scoreOfRolls[3]++;
+                        break;
+                    case "5":
+                        scoreOfRolls[4]++;
+                        break;
+                    case "6":
+                        scoreOfRolls[5]++;
+                        break;
+                }//switch-case
+            }//for
+
+            currentDeviation = await Math.max(...scoreOfRolls) / Math.min(...scoreOfRolls);
+
+            if (scoreOfRolls[0] == 0) {
+                cosole.log(`\x1B[33mSomething went wrong, please recheck your code\x1b[0m`)
+            } else if (currentDeviation > 1 + maxDeviation / 100) {
+                console.log(`Current deviation is \x1b[33m${currentDeviation}\x1b[0m at \x1b[33m${currentQtty}\x1b[0m, \x1b[35mnot enough.\x1b[0m Let's increase quantity to \x1b[33m${currentQtty = Math.round(currentQtty * currentDeviation * howQuick)}\x1b[0m`)
+                if (currentQtty < maxQtty) {
+                    console.log(`...`)
+                } else {
+                    console.log(`\x1b[35mOops, to much... Now we can try only the maximum: \x1b[1m${maxQtty}\x1b[0m     
+...`)
+                }
+            } else {
+                console.log(`\x1B[32mCurrent deviation is OK: \x1B[1m${currentDeviation}\x1b[0m\x1b[32m at \x1b[1m${currentQtty}\x1b[0m\x1b[33m which delivers \x1b[0m[${scoreOfRolls}])\x1B[0m`)
+            }
+        }//while
+    });//Get all rolls
 });
